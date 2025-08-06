8faa9d950b1966f4283216a7d22064a3a9421ce9
z <1372234588@QQ.COM>
2025-08-06 20:10:56 +0800

    Update Chapter_8.scala

--- Chapter_8.scala
+++ Chapter_8.scala
@@ -1,45 +1,67 @@
-/*import cats.effect.IO
-import cats.implicits.**/
-import java.io.IO
+import cats.effect.IO
+import cats.implicits.*
+import cats.effect.unsafe.implicits.global
+
+import scala.compiletime.ops.string.Length
 import scala.util.Try
 
-/*
-def calendarEntries(name:String):IO(List[MeetingTime])={
-  IO.delay(calendarEntriesApiCall(name))
-}*/
+//import ch08_SchedulingMeetings.calendarEntriesApiCall
 object Main {
   case class MeetingTime(startHour: Int, endHour: Int)
 
-  def calendarEntriesApiCall(name: String): IO[List[MeetingTime]] = {
+  def calendarEntriesApiCall(name: String): List[MeetingTime] = {
+    import scala.jdk.CollectionConverters._
+    Main.calendarEntriesApiCall(name).toList.map(mt =>
+      MeetingTime(mt.startHour, mt.endHour) // convert from Java to Scala
+    )
+  }
+
+
+  def calendarEntries(name: String): IO[List[MeetingTime]] =
     IO.delay(calendarEntriesApiCall(name))
+
+  def createMeeting(names: List[String], meeting: MeetingTime): IO[Unit] = {
+    IO.delay(createMeeting(names, meeting))
   }
 
-  def createMeetings(names: List[String], meeting: MeetingTime): IO[Unit] = {
-    IO.delay(createMeetings(names, meeting))
-  }
+  def scheduledMeetings(attendees:List[String]): IO[List[MeetingTime]] = {
+    attendees
+      .map(attendee=>retry(calendarEntries(attendee),10))
+      .sequence
+      .map(_.flatten)
+  }a
 
-  def schedualdMeetings(person1: String, person2: String): IO[List[MeetingTime]] =
-    for {
-      person1Entries <- calendarEntriesApiCall(person1)
-      person2Entries <- calendarEntriesApiCall(person2)
-    } yield person1Entries.appendedAll(person2Entries)
-
-  def meetingsOverlap(meeting1: MeetingTime, meeting2: MeetingTime): Boolean =
+  def meetingOverlaps(meeting1: MeetingTime, meeting2: MeetingTime): Boolean =
     meeting1.endHour > meeting2.startHour && meeting2.endHour > meeting1.startHour
 
-  def possibleMeetings(existingMeetins: List[MeetingTime], statrHour: Int, endHour: Int, lengthHours: Int): List[MeetingTime] = {
-    val slots = List.range(statrHour, endHour - lengthHours + 1).map(statrHour => MeetingTime(startHour, statrHour + lengthHours))
-    slots.fliter(slots => existingMeetins.forall(meeting =>! meetingsOverlap(meeting, slots)))
+  def possibleMeetings(
+                        existingMeetings: List[MeetingTime],
+                        startHour: Int,
+                        endHour: Int,
+                        lengthHours: Int
+                      ): List[MeetingTime] = {
+    val slots =
+      List.range(startHour, endHour - lengthHours + 1).map(startHour => MeetingTime(startHour, startHour + lengthHours))
+    slots.filter(slot => existingMeetings.forall(meeting => !meetingOverlaps(meeting, slot)))
   }
 
-  def schudule(person1: String, person2: String, lengthHours: Int): IO[Option[MeetingTime]] =
+  def shedule(attendees:List[String], lengthHours: Int): IO[Option[MeetingTime]] =
     for {
-      existingMeetings <- schedualdMeetings(person1, person2)
-      meetings = possibleMeetings(existingMeetings, 8, 16, lengthHours)
-    } yield meetings.headOption
+      existingMeetinfgs <- scheduledMeetings(attendees)
+      meetins = possibleMeetings(existingMeetinfgs, 8, 16, lengthHours)
+      possibleMeeting = meetins.headOption
+      _ <- possibleMeeting match {
+        case Some(meeting) => createMeeting(attendees, meeting)
+        case None => IO.unit
+      } 
+    } yield possibleMeeting
+  def retry[A](action:IO[A],maxRetries:Int):IO[A]=
+    List.range(0,maxRetries)
+      .map(_=>action)
+      .foldLeft(action)((program,retryAction)=>{
+        program.orElse(retryAction)
+      })
+      
   def main(args: Array[String]): Unit = {
-    val peogram=schudule("Alce","Bob",1)
-    println(peogram)
-    println("明天再补全")
   }
 }
