c6c1481bfd918bbb5cdec2ec904cce23d0f4ea7d
z <1372234588@QQ.COM>
2025-08-12 16:04:47 +0800

    Update Chapter_11.scala

--- Chapter_11.scala
+++ Chapter_11.scala
@@ -1,14 +1,11 @@
-现在还没学完
 import Main.model.PopCultureSubject.{Artist, Movie}
-import cats.effect._
+import cats.Traverse.nonInheritedOps.toTraverseOps
+import cats.effect.*
 import org.apache.jena.rdfconnection.{RDFConnection, RDFConnectionRemote}
 import org.apache.jena.query.{QueryExecution, QueryFactory, QuerySolution}
-import scala.io.Codec.fallbackSystemCodec.name
 
-import cats.effect.unsafe.implicits.global
-//import ch11_WikidataDataAccess.getSparqlDataAccess
-import scala.concurrent.duration._
 import scala.jdk.javaapi.CollectionConverters.asScala
+import cats.effect.unsafe.implicits.global
 
 object Main {
 
@@ -16,70 +13,66 @@ object Main {
   import AttractionOrdering._
 
   object model {
-
-    opaque; type LocationID = String
-
+    opaque type LocationID = String
     object LocationID {
       def apply(value: String): LocationID = value
-
-      extension (a: LocationID) ;def value: String = a
+      extension (a: LocationID) def value: String = a
     }
 
     case class Location(id: LocationID, name: String, population: Int)
-
     case class Attraction(name: String, description: Option[String], location: Location)
 
     enum PopCultureSubject {
-      case Artist(name: String, followers: Int)=>
-      case Movie(name: String, boxOffice: Int)=>
+      case Artist(name: String, followers: Int)
+      case Movie(name: String, boxOffice: Int)
     }
 
     case class TravelGuide(attraction: Attraction, subject: List[PopCultureSubject])
-
-
   }
 
   enum AttractionOrdering {
-    case ByName =>
-    case ByLocationPopulation =>
+    case ByName
+    case ByLocationPopulation
   }
 
   trait DataAccess {
     def findAttractions(name: String, ordering: AttractionOrdering, limit: Int): IO[List[Attraction]]
-
     def findArtistsFrom(location: LocationID, limit: Int): IO[List[Artist]]
-
     def findMovieAboutLocation(location: LocationID, limit: Int): IO[List[Movie]]
   }
 
   def travelGuide(data: DataAccess, attractionname: String): IO[Option[TravelGuide]] =
     for {
-      attraction <- data.findAttractions(attractionname, ByLocationPopulation, 1)
-      guide <- attraction.headOption match {
-        case None => IO.pure(None)
-        case Some(attraction) => for {
-          artist <- data.findArtistsFrom(attraction.location.id, 2)
-          movie <- data.findMovieAboutLocation(attraction.location.id, 2)
-        } yield Some(TravelGuide(attraction, artist.appendedAll(movie)))
-      }
-    } yield guide
+      attraction <- data.findAttractions(attractionname, ByLocationPopulation, 3)
+      guides <- attraction
+        .map(attraction => for {
+          artists <- data.findArtistsFrom(attraction.location.id, 2)
+          movies <- data.findMovieAboutLocation(attraction.location.id, 2)
+        } yield TravelGuide(attraction, artists.appendedAll(movies))
+        )
+        .sequence
+    } yield guides.sortBy(guideScore).reverse.headOption
 
   val getConnection: IO[RDFConnection] = IO.delay(
-    RDFConnectionRemote.create // we will make it better, see at the end
+    RDFConnectionRemote.create
       .destination("https://query.wikidata.org/")
       .queryEndpoint("sparql")
       .build
   )
 
-  def execQuery(getConnection: IO[RDFConnection], query: String): IO[List[QuerySolution]] = {
-    getConnection.flatMap(c =>
-      IO.delay(
-        asScala(c.query(QueryFactory.create(query)).execSelect()).toList
-      )
-    )
-  }
+  def createExecution(connection: RDFConnection, query: String): IO[QueryExecution] =
+    IO.blocking(connection.query(QueryFactory.create(query)))
 
-  def parseAttraction(s: QuerySolution): IO[Attraction] = {
+  def closeExecution(execution: QueryExecution): IO[Unit] =
+    IO.blocking(execution.close())
+
+ 
+  def execQuery(connection: RDFConnection)(query: String): IO[List[QuerySolution]] =
+    val executionResource: Resource[IO, QueryExecution] =
+      Resource.make(createExecution(connection, query))(closeExecution)
+    executionResource.use(execution => IO.blocking(asScala(execution.execSelect()).toList))
+
+  def parseAttraction(s: QuerySolution): IO[Attraction] =
     IO.delay(
       Attraction(
         name = s.getLiteral("attractionLabel").getString,
@@ -91,7 +84,6 @@ object Main {
         )
       )
     )
-  }
 
   def findAttractions(name: String, ordering: AttractionOrdering, limit: Int): IO[List[Attraction]] = {
     val orderBy = ordering match {
@@ -101,40 +93,37 @@ object Main {
 
     val query =
       s"""
-                      PREFIX wd: <http://www.wikidata.org/entity/>
-                      PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-                      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                      PREFIX schema: <http://schema.org/>
-                      SELECT DISTINCT ?attraction ?attractionLabel ?description ?location ?locationLabel ?population WHERE {
-                        ?attraction wdt:P31 wd:Q570116;
-                                    rdfs:label ?attractionLabel;
-                                    wdt:P131 ?location.
-                        FILTER(LANG(?attractionLabel) = "en").
+         PREFIX wd: <http://www.wikidata.org/entity/>
+         PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+         PREFIX schema: <http://schema.org/>
+         SELECT DISTINCT ?attraction ?attractionLabel ?description ?location ?locationLabel ?population WHERE {
+           ?attraction wdt:P31 wd:Q570116;
+                       rdfs:label ?attractionLabel;
+                       wdt:P131 ?location.
+           FILTER(LANG(?attractionLabel) = "en").
 
-                        OPTIONAL {
-                          ?attraction schema:description ?description.
-                          FILTER(LANG(?description) = "en").
-                        }
+           OPTIONAL {
+             ?attraction schema:description ?description.
+             FILTER(LANG(?description) = "en").
+           }
 
-                        ?location wdt:P1082 ?population;
-                                  rdfs:label ?locationLabel;
-                        FILTER(LANG(?locationLabel) = "en").
+           ?location wdt:P1082 ?population;
+                     rdfs:label ?locationLabel;
+           FILTER(LANG(?locationLabel) = "en").
 
-                        FILTER(CONTAINS(?attractionLabel, "$name")).
-                      } ORDER BY $orderBy LIMIT $limit
-                      """
+           FILTER(CONTAINS(?attractionLabel, "$name")).
+         } ORDER BY $orderBy LIMIT $limit
+       """
 
     for {
-      solutions <- execQuery(getConnection, query)
+      connection <- getConnection
+      solutions  <- execQuery(connection)(query) // ✅ 修正：调用方式匹配
       attractions <- solutions.traverse(parseAttraction)
     } yield attractions
   }
 
-  def execQuery(connection: RDFConnection)(query: String): IO[List[QuerySolution]] = IO.blocking(
-    asScala(connection.query(QueryFactory.create(query)).execSelect()).toList
-  )
-
-  def getSparqlDataAccess(execQuery: String => IO[List[QuerySolution]]): DataAccess = new DataAccess {
+  def getSparqlDataAccess(execQueryFn: String => IO[List[QuerySolution]]): DataAccess = new DataAccess {
     val prefixes =
       """
         |PREFIX wd: <http://www.wikidata.org/entity/>
@@ -172,13 +161,12 @@ object Main {
            |""".stripMargin
 
       for {
-        solutions <- execQuery(query)
+        solutions <- execQueryFn(query)
         attractions <- IO.delay(
           solutions.map(s =>
-            Attraction( // introduce named parameters
+            Attraction(
               name = s.getLiteral("attractionLabel").getString,
-              description =
-                if (s.contains("description")) Some(s.getLiteral("description").getString) else None,
+              description = if (s.contains("description")) Some(s.getLiteral("description").getString) else None,
               location = Location(
                 LocationID(s.getResource("location").getLocalName),
                 s.getLiteral("locationLabel").getString,
@@ -190,7 +178,8 @@ object Main {
       } yield attractions
     }
 
-    def findArtistsFromLocation(locationId: LocationID, limit: Int): IO[List[Artist]] = {
+    
+    def findArtistsFrom(locationId: LocationID, limit: Int): IO[List[Artist]] = {
       val query =
         s"""
            |$prefixes
@@ -206,17 +195,16 @@ object Main {
            |""".stripMargin
 
       for {
-        solutions <- execQuery(query)
-        artists <-
-          IO.delay(
-            solutions.map[Artist](s =>
-              Artist(name = s.getLiteral("artistLabel").getString, followers = s.getLiteral("followers").getInt)
-            )
+        solutions <- execQueryFn(query)
+        artists <- IO.delay(
+          solutions.map[Artist](s =>
+            Artist(name = s.getLiteral("artistLabel").getString, followers = s.getLiteral("followers").getInt)
           )
+        )
       } yield artists
     }
 
-    def findMoviesAboutLocation(locationId: LocationID, limit: Int): IO[List[Movie]] = {
+    def findMovieAboutLocation(locationId: LocationID, limit: Int): IO[List[Movie]] = {
       val query =
         s"""
            |$prefixes
@@ -233,7 +221,7 @@ object Main {
            |""".stripMargin
 
       for {
-        solutions <- execQuery(query)
+        solutions <- execQueryFn(query)
         movies <- IO.delay(
           solutions.map[Movie](s =>
             Movie(name = s.getLiteral("subjectLabel").getString, boxOffice = s.getLiteral("boxOffice").getInt)
@@ -243,24 +231,32 @@ object Main {
     }
   }
 
-  def guideScore(guide: TravelGuide): Int ={
+  def guideScore(guide: TravelGuide): Int = {
     val descriptionScore = guide.attraction.description.map(_ => 30).getOrElse(0)
     val quantityScore = Math.min(40, guide.subject.size * 10)
-    val totalFollwers = guide.subject
-      .map( _match {
-        case Artist(_, followers) => followers
-        case _ => 0
-      })
-        .sum
-    val totalBoxOffice =guide.subject
-      .map(_match{
-        case Movie(_,boxOffice)=>boxOffice
-        case _ =>
-      })
-      .sum
+    val totalFollowers = guide.subject.collect { case Artist(_, f) => f }.sum
+    val totalBoxOffice = guide.subject.collect { case Movie(_, b) => b }.sum
 
+    val followersScore = Math.min(15, totalFollowers / 100000)
+    val boxOfficeScore = Math.min(15, totalBoxOffice / 10000000)
+    descriptionScore + quantityScore + followersScore + boxOfficeScore
   }
-  def main(args: Array[String]): Unit = {
 
+  def main(args: Array[String]): Unit = {
+    val connectionResource: Resource[IO, RDFConnection] =
+      Resource.make(IO.blocking(
+        RDFConnectionRemote.create
+          .destination("https://query.wikidata.org/")
+          .queryEndpoint("sparql")
+          .build
+      ))(connection => IO.blocking(connection.close()))
+
+    val program: IO[Option[TravelGuide]] =
+      connectionResource.use { connection =>
+        val wikidata = getSparqlDataAccess(execQuery(connection))
+        travelGuide(wikidata, "Yellowstone")
+      }
+
+    println(program.unsafeRunAsync(_ => ()))
   }
 }
